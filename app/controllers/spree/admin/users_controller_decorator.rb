98eb658c51d03a1c69899470dfde4656729cbe1a
Paweł Świątkowski <pawel.swiatkowski@juul.com>
2019-12-03 10:07:52 +0100

    Lazy load UsersController decorator

--- app/controllers/spree/admin/users_controller_decorator.rb
+++ app/controllers/spree/admin/users_controller_decorator.rb
@@ -1,11 +1,13 @@
-Spree::Admin::UsersController.class_eval do
-  def avalara_information
-    if request.put?
-      if @user.update_attributes(user_params)
-        flash.now[:success] = Spree.t(:account_updated)
-      end
-    end
+ActiveSupport.on_load('Spree::Admin::UsersController') do
+	Spree::Admin::UsersController.class_eval do
+		def avalara_information
+			if request.put?
+				if @user.update_attributes(user_params)
+					flash.now[:success] = Spree.t(:account_updated)
+				end
+			end
 
-    render :avalara_information
-  end
+			render :avalara_information
+		end
+	end
 end
